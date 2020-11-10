95c882abef2d6263a5a77db36690ed3025297570
victorlcs <victor.loh95@gmail.com>
2020-11-10 20:18:48 +0800

    fix mouse click

--- src/app/components/dict-panel-card/dict-panel-card.component.ts
+++ src/app/components/dict-panel-card/dict-panel-card.component.ts
@@ -48,7 +48,9 @@ export class DictPanelCardComponent implements OnInit {
         });
     }
   }
-
+  onFocus() {
+    this.panelServices.setCurPanel = this.cardIndex.toString();
+  }
   //Hostlistener
   @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
