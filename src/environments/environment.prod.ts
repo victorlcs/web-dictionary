ba89bc95e784657308599cc9fa7209a960ca87af
victorlcs <victor.loh95@gmail.com>
2020-11-10 19:38:56 +0800

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+  apiHeader: {
+    host: 'x-rapidapi-host',
+    hostValue: 'mashape-community-urban-dictionary.p.rapidapi.com',
+    key: 'x-rapidapi-key',
+    keyValue: 'a793f62aacmshb9779ee844741f5p127045jsn24234269d0ff',
+  },
 };
